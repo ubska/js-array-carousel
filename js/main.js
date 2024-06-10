e5fed524ef5b78384a33e2a170e983d6a49c4be2
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-10 17:26:56 +0200

    add img item

--- js/main.js
+++ js/main.js
@@ -8,3 +8,25 @@ const imageArray = [
     "img /04.webp",
     "img /05.webp"
 ];
+
+// seleziono il contenitore
+const itemsContainer = document.querySelector(".items-container")
+
+
+// estrapolo gli elementi dall'array img
+for (let i = 0; i < imageArray.length; i++){
+    itemImg = imageArray[i];
+
+    // creare ad ogni iterazione l'elemento da inserire
+    let itemContent = `
+        <div class="item">
+            <img src="${itemImg}">
+        </div>`
+    console.log(itemContent);
+    //  inseriamo il nuovo elemento nel contenitore selezionato precedentemente
+    itemsContainer.innerHTML += itemContent;    
+}
+
+// seleziono tutti gli elementi in pagina
+const items = document.getElementsByClassName("item");
+console.log(items);
