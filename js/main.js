444e1e734d03059f36280939fadf160f1f229df0
Oleksandra Ubska <alessandraubska@gmail.com>
2024-06-10 16:59:46 +0200

    add array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 // Dato un array contenente una lista di cinque immagini, creare un carosello
+
+// inserire da js i contenuti del carosello ( array )
+const imageArray = [
+    "img /01.webp",
+    "img /02.webp",
+    "img /03.webp",
+    "img /04.webp",
+    "img /05.webp"
+];
